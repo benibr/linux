f33aa090e3737db9696aff4ea444d19a662408b9
Benedikt Braunger <bb@emlix.com>
2022-11-21 18:57:12 +0100

    try to be compatible, reserved--

--- include/uapi/linux/btrfs_tree.h
+++ include/uapi/linux/btrfs_tree.h
@@ -676,7 +676,7 @@ struct btrfs_root_item {
 	struct btrfs_timespec stime;
 	struct btrfs_timespec rtime;
 	struct btrfs_timespec dtime;
-	__le64 reserved[8]; /* for future */
+	__le64 reserved[6]; /* for future */
 } __attribute__ ((__packed__));
 
 /*
