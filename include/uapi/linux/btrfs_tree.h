0aeb36962a5fa0fee0b22947e1685303dcc5265b
Benedikt Braunger <bb@emlix.com>
2022-11-21 19:10:24 +0100

    added stub to fillup

--- include/uapi/linux/btrfs_tree.h
+++ include/uapi/linux/btrfs_tree.h
@@ -676,6 +676,7 @@ struct btrfs_root_item {
 	struct btrfs_timespec stime;
 	struct btrfs_timespec rtime;
 	struct btrfs_timespec dtime;
+	__le32 stub; 
 	__le64 reserved[6]; /* for future */
 } __attribute__ ((__packed__));
 
